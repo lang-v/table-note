add8103c6b1e885ca34ee1cd86e3d1ba2f33f351
lang <ws2240@qq.com>
2022-10-30 00:31:27 +0800

    增加输入框

--- js/background.js
+++ js/background.js
@@ -4,20 +4,3 @@ chrome.runtime.onInstalled.addListener(() => {
     chrome.storage.sync.set({ color });
     console.log('Default background color set to %cgreen', `color: ${color}`);
 });
-
-// 注入当前激活标签页面
-// async function insertCSS() {
-//     const css = 'body { background-color: ${color}; }';
-//     let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
-//     console.log(tab);
-//     chrome.scripting.insertCSS(
-//         {
-//             target: { tabId: tab.id },
-//             css: css
-//         }
-//     )
-// }
-
-
-
-// insertCSS();
